be18ef6ee08a7623fed9a710bc12ef15c607472d
Raditya Saskara <saskaradit@gmail.com>
2022-11-10 20:22:35 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView,Alert, StyleSheet, Platform, Button } from 'react-native';
 
+
+// View -> UI View
 export default function App() {
+  const handlePress = () => {
+    console.log("text pressed")
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button color="grey" title='Click Me' 
+      onPress={()=>Alert.alert('Oops', 'Button Clicked',[
+        {text: 'Go back' , onPress: () => console.log('sure go back')},
+        {text: 'Go ahead', onPress: () => console.log('lets gooo')}
+      ])}></Button>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4f4f4',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
 });
